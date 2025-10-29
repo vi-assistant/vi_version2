28bf01a1d6408661f05a74a6af34ce670314f613
Igbana <igbanaaondosoo13@gmail.com>
2025-10-29 16:18:16 +0100

    Safe commit

--- lib/views/splash/splash.dart
+++ lib/views/splash/splash.dart
@@ -1,28 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:vi_assistant/utils/utils.dart';
 
-
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          spacing: 36,
-          children: [
-            Text("Welcome to VI Assistant", style: Style.heading),
-            ElevatedButton(
-              onPressed: () => Navigator.of(context).pushNamed(Routes.login),
-              child: Text("Login to existing account"),
-            ),
-            ElevatedButton(
-              onPressed: () => Navigator.of(context).pushNamed(Routes.signup),
-              child: Text("Create new account"),
-            ),
-          ],
+        child: SizedBox(
+          width: size.width * 0.5,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            spacing: 36,
+            children: [
+              Text(
+                "Welcome to VI Assistant",
+                style: Style.heading,
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 12),
+              ElevatedButton(
+                onPressed: () => Navigator.of(context).pushNamed(Routes.login),
+                child: Text("Login to existing account"),
+              ),
+              ElevatedButton(
+                onPressed: () => Navigator.of(context).pushNamed(Routes.signup),
+                child: Text("Create new account"),
+              ),
+            ],
+          ),
         ),
       ),
     );
